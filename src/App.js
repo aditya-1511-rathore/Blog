c1483a04df8d187471ee180e7a1432b8a1cc8798
Aditya Singh <aditya.l.ls65@gmail.com>
2023-07-04 01:03:55 +0530

    login button and searh bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
+      <nav className="App-navbar">
+        <div className="App-search">
+          <input type="text" placeholder="Search" />
+          <button type="submit">Search</button>
+        </div>
+        <div className="App-login">
+          <button>Login</button>
+        </div>
+      </nav>
+
+      <nav className="App-sidebar">
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
